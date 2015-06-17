13556d9f51cfadeead24fa9d8bb7cc360e286a0a
Zihao Wang <wzhdev@gmail.com>
2015-06-17 23:22:27 +0800

    Update time every second

--- src/main.c
+++ src/main.c
@@ -3,11 +3,35 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer that persists across multiple calls
+  static char buffer[] = "00:00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00:00"), "%H:%M:%S", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00:00"), "%I:%M:%S", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
 
   layer_add_child(window_get_root_layer(window),
                   text_layer_get_layer(s_time_layer));
@@ -30,6 +54,8 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+  update_time();
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit() {
