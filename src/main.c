38478b5808d5b5c2245948dda4c202e177d94fd8
Zihao Wang <wzhdev@gmail.com>
2015-06-17 22:39:00 +0800

    Add text

--- src/main.c
+++ src/main.c
@@ -1,13 +1,21 @@
 #include <pebble.h>
   
 static Window *s_main_window;
+static TextLayer *s_time_layer;
 
 static void main_window_load(Window *window) {
+  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
 
+  layer_add_child(window_get_root_layer(window),
+                  text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
 }
   
 static void init() {
