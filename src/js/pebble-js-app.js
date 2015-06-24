57f862e0325c23dc07539a411d8a7032de324079
Zihao Wang <wzhdev@gmail.com>
2015-06-24 09:48:27 +0800

    Get weather and display in console

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -2,6 +2,9 @@
 Pebble.addEventListener('ready',
   function(e) {
     console.log('PebbleKit JS ready!');
+
+    // Get the initial weather
+    getWeather();
   }
 );
 
@@ -11,3 +14,45 @@ Pebble.addEventListener('appmessage',
     console.log('AppMessage received!');
   }
 );
+
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // We will request the weather here
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +
+      pos.coords.latitude + '&lon=' + pos.coords.longitude;
+  xhrRequest(url,
+             "GET",
+             function(responseText){
+               // responseText contains a JSON object with weather info
+               var json = JSON.parse(responseText);
+
+               // Temperature in Kelvin requires adjustment
+               var temperature = json.main.temp;
+               console.log('Temperature is ' + temperature);
+
+               // Conditions
+               var conditions = json.weather[0].main;
+               console.log('Conditions are ' + conditions);
+             }
+            );
+}
+
+function locationError(err) {
+  console.log('Error requesting location!');
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
